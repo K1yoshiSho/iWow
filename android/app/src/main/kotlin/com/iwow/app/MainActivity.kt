eb72c266dcd07449231b81a4af179b9d20c2474c
Yelaman Yelmuratov <e99xxx@gmail.com>
2023-01-08 13:48:53 +0600

    fix bugs with first-open crush

---
+++ android/app/src/main/kotlin/com/iwow/app/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.iwow.app
+
+import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+class MainActivity: FlutterActivity() {
+}
